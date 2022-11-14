f7c020130c6eab2e5ecae25f301c349551ff23bb
Paweł Beśka <raz4r@interia.pl>
2022-11-14 15:20:08 +0200

    Update ProductInterface.php

--- src/Interfaces/ProductInterface.php
+++ src/Interfaces/ProductInterface.php
@@ -4,8 +4,12 @@ declare(strict_types=1);
 
 namespace Bavix\Wallet\Interfaces;
 
+use Jenssegers\Mongodb\Eloquent\Model;
+
 interface ProductInterface extends Wallet
 {
+    public function getCountry(): Model;
+
     public function getAmountProduct(Customer $customer): int|string;
 
     /**
